46eb089fa076be3e3c0b3fe6c01930cdb5ceeb18
unknown <nwessel429@gmail.com>
2019-10-18 08:09:08 -0500

    added files and npm

---
+++ src/Components/Auth/Register.js
@@ -0,0 +1,7 @@
+const getEvents = value => {
+  fetch(`https://api.mysportsfeeds.com/v2.1/pull/nhl/players.team=${value}`, {
+    headers: {
+        "Authorization": "Basic " + btoa({ea2d0048ce5a4a16ae0083d4d7} + ":" + MYSPORTSFEEDS)
+    }
+  });
+};
